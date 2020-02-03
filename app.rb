7fd724fe49205adbe480ebfecf3386470bdec46d
Josh Blumberg <jlblumberg@gmail.com>
2020-02-03 15:57:56 +0000

    Add param to controller

--- app.rb
+++ app.rb
@@ -15,8 +15,13 @@ get '/author' do
   "Josh Blumberg"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
 
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
